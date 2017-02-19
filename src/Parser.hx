197f00869f61daab8b734f080ad3d8f347072cc5
oscarcs <nxtrafalgar@yahoo.co.nz>
2017-02-20 00:28:12 +1300

    Add infix operator parsing (without regard to precedence etc).

--- src/Parser.hx
+++ src/Parser.hx
@@ -39,14 +39,44 @@ class IdentNode extends Node
 class PrefixNode extends Node
 {
     public var child:Node;
-    override public function new(value:String, operand:Node)
+    override public function new(value:String, child:Node)
     {
         super(value);
-        this.children.push(operand);
-        operand.parent = this;
+        this.children.push(child);
         this.child = this.children[0];
+        this.child.parent = this;
     }
 }
+
+class InfixNode extends Node
+{
+    public var left:Node;
+    public var right:Node;
+    override public function new(value:String, left:Node, right:Node)
+    {
+        super(value);
+        this.children.push(left);
+        this.children.push(right);
+        this.left = this.children[0];
+        this.right = this.children[1];
+        this.left.parent = this;
+        this.right.parent = this;        
+    }
+}
+
+class PostfixNode extends Node
+{
+    public var child:Node;
+    override public function new(value:String, child:Node)
+    {
+        super(value);
+        this.children.push(child);
+        this.child = this.children[0];
+        this.child.parent = this;
+    }
+}
+
+typedef Parselet = Token->Node->Node;
 
 class Parser
 {
@@ -54,22 +84,29 @@ class Parser
     private var pos = -1;
     private var c:Token;
 
-    private var parseFuncs = new Map<String, Token->Node>();
+    private var prefixFuncs = new Map<String, Parselet>();
+    private var infixFuncs = new Map<String, Parselet>();
 
     public function new(tokens:Array<Token>) 
     {
         this.tokens = tokens;
 
         // register the grammar:
-        register("IDENTIFIER", function(token) { 
-            return new IdentNode(token.lexeme);
-        });
+        register(
+            "IDENTIFIER",
+            function(token:Token, left:Node) { 
+                return new IdentNode(token.lexeme);
+            }, 
+            "none"
+        );
         registerPrefix("ADD");
         registerPrefix("SUBTRACT");
         registerPrefix("COMPLEMENT");
         registerPrefix("NOT");
-
-
+        registerInfix("ADD");
+        registerInfix("SUBTRACT");
+        registerInfix("MULTIPLY");
+        registerInfix("DIVIDE");
     }
 
     public function parse():Node
@@ -77,18 +114,43 @@ class Parser
         return expression();
     }
 
-    private function register(type:String, f:Token->Node)
+    private function register(type:String, f:Parselet, affix:String)
     {
-        parseFuncs[type] = f;
+        if (affix == "infix")
+        {
+            infixFuncs[type] = f;
+        }
+        else
+        {
+            prefixFuncs[type] = f;
+        }
     }
 
     private function registerPrefix(type:String)
     {  
-        var f = function(token) {
+        var f = function(token:Token, left:Node) {
             var operand:Node = expression();
             return new PrefixNode(token.type, operand);
         };
-        register(type, f);
+        register(type, f, "prefix");
+    }
+
+    private function registerInfix(type:String)
+    {
+        var f = function(token:Token, left:Node) {
+            var right = expression();
+            return new InfixNode(token.type, left, right);
+        };
+        register(type, f, "infix");
+    }
+
+    private function registerPostfix(type:String)
+    {
+        var f = function(token:Token, left:Node) {
+            return new PostfixNode(token.type, left);
+        };
+        // postfix ops are actually 'infix':
+        register(type, f, "infix");
     }
 
     private function advance()
@@ -97,13 +159,39 @@ class Parser
         c = tokens[pos];
     }
 
+    private function lookahead()
+    {
+        return tokens[pos + 1];
+    }
+
     private function expression():Node
     {
         advance();
-        var prefix = parseFuncs[c.type];
-        
-        //@@ERROR
+        if (prefixFuncs.exists(c.type))
+        {
+            var prefix = prefixFuncs[c.type];
 
-        return prefix(c);
+            var left = prefix(c, null);
+
+            var token = lookahead();
+
+            if (infixFuncs.exists(token.type))
+            {
+                var infix = infixFuncs[token.type];
+                
+                advance();
+                return infix(token, left);
+            }
+            else 
+            {
+                return left;
+            }
+        }
+        else
+        {
+            return null;
+        }
+
+        return null;
     }
 }
