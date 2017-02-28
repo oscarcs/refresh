14a606c05b8b0f86d180f2ab61cc2733be48b949
oscarcs <nxtrafalgar@yahoo.co.nz>
2017-03-01 00:49:37 +1300

    First steps towards code generation.

--- src/backends/JSGenerator.hx
+++ src/backends/JSGenerator.hx
@@ -1,5 +1,7 @@
 package backends;
 
+import Node;
+
 class JSGenerator implements IGenerator
 {
     private var rootNode:Node;
@@ -13,6 +15,38 @@ class JSGenerator implements IGenerator
 
     public function generate():String
     {
-        return "";
+        var string = '';
+
+        string += generateNode(rootNode);
+
+        Files.write(outputPath, string);
+
+        return string;
+    }
+
+    private function generateNode(node:Node):String
+    {
+        //@@CLEANUP: can we do this w/o casting?
+        var str:String = '';
+        switch(Type.getClass(node))
+        {
+            case RootNode:
+                var n = cast(node, RootNode);
+                str = n.children.map(generateNode).join('\n');
+
+            case AssignNode:
+                var n = cast(node, AssignNode);
+                str = generateNode(n.left) + ' = ' + generateNode(n.right) + ';';
+
+            case IdentNode:
+                var n = cast(node, IdentNode);
+                str = n.value;
+
+            case IntNode:
+                var n = cast(node, IntNode);
+                str = n.value + '';
+        }
+
+        return str;
     }
 }
