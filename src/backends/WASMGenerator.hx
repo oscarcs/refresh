8de23c7993f509852ee650bd38ce1d0fe513f209
oscarcs <nxtrafalgar@yahoo.co.nz>
2017-04-30 23:47:51 +1200

    Improve structure of WASM backend.

--- src/backends/WASMGenerator.hx
+++ src/backends/WASMGenerator.hx
@@ -2,9 +2,31 @@ package backends;
 
 import Node;
 
+typedef W_Param = {
+    name:String,
+    type:W_Type
+};
+
+typedef W_Export = {
+    // Name of the export, i.e. the name that
+    // will be exposed to the calling environment.
+    name:String,
+    // Internal reference name for the 
+    reference:String
+};
+
+enum W_Type {
+    I32;
+}
+
 class WASMGenerator implements IGenerator
 {
     private var rootNode:Node;
+
+    // Array of exported functions.
+    // We defer exporting to the end of the
+    // textual representation.
+    private var exports:Array<W_Export> = [];
 
     public function new(rootNode:Node)
     {
@@ -46,25 +68,68 @@ class WASMGenerator implements IGenerator
     {
         var str = "";
         str += '(module \n'; 
-        str += generateFunction("main");
+        str += generateFunction("main", []) + '\n';
+        
         //@@TODO: generate other functions
+
+
+        // Generate exports
+        for (export in exports)
+        {
+            str += generateExport(export) + '\n';
+        }
+
         str += ')';
         return str;
     }
 
-    private function generateFunction(name:String):String
+    /**
+     *  Generate the code for a function.
+     *  @param name Internal name of a function.
+     *  @param args Array of arguments to a function.
+     *  @return String
+     */
+    private function generateFunction(name:String, args:Array<W_Param>):String
     {
+        //@@TODO: Determine whether this function needs to be exported:
+        var isExport:Bool = name == "main";
+
+        //@@TODO: Determine the export name:
+        var exportName:String = name;
+
         var str = "";
-        str += '(func ' + generateExport(name) + '\n';
+        str += '(func $' + name; 
+        for (arg in args)
+        {
+            str += '('; 
+        }
+        str += '\n';
+
         //@@TODO: generate function body
+        
         str += ")";
+
+        if (isExport)
+        {
+            addExport(name, exportName);
+        }
+
         return str;
     }
 
-    private function generateExport(name:String):String
+    private function addExport(name:String, reference:String):Void
+    {
+        this.exports.push({ name: name, reference: reference });
+    }
+
+    /**
+     *  Generate an export for a function.
+     *  @param name Name of exported function
+     */
+    private function generateExport(export:W_Export):String
     {
         var str = "";
-        str += "(export \"" + name + "\")";
+        str += '(export "' + export.name + '" (func $' + export.reference + '))';
         return str;
     }
 
