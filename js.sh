1c8a4c711fd57f54da31e53889dbb9a4c42fd8fa
oscarcs <nxtrafalgar@yahoo.co.nz>
2017-05-21 21:13:35 +1200

    Update build scripts to reflect compiler interface changes.

--- js.sh
+++ js.sh
@@ -1,4 +1,4 @@
 # Build and test the JS target.
 haxe build.hxml && 
-node bin/refresh.js -js -t3 test/test.prog test/test.js &&
+node bin/refresh.js -js --debug test/test.prog test/test.js &&
 node test/test.js
--- wasm.sh
+++ wasm.sh
@@ -1,8 +1,8 @@
 # Build and test the WebAssembly target.
 haxe build.hxml &&
 # Compile the code to a WebAssembly AST:
-node bin/refresh.js -wasm -t3 test/test.prog test/test.wast &&
+node bin/refresh.js -wasm --debug test/test.prog test/test.wast &&
 # Compile the AST to bytecode:
-wasm -d test/test.wast -o test/test.wasm
+wasm -d test/test.wast -o test/test.wasm &&
 # Run the WebAssembly code:
 node8 --expose-wasm wasm.js
